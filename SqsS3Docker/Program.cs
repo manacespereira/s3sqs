ee26aab47bbc2423294e2526b9d4717ac6bc04e9
Manacés Pereira da Silva Neto <manaces.silva@amcom.com.br>
2018-05-23 18:17:02 -0300

    todos arquivos

---
+++ SqsS3Docker/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqsS3Docker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqsHelper = new SQSHelper("AKIAIRJXFRCBKAQ55WSA", "lPDJrZBUjnNdJsIkcshavciTOiibbaMzBh03MqBh", RegionEndpoint.SAEast1);
+
+            sqsHelper.CreateQueue("TESTEQUEUE").Wait();
+
+            var a = sqsHelper.GetQueue().Result;
+
+            sqsHelper.SendMessage(a.QueueUrls.First(), $"{DateTime.Now.ToString()}").Wait();
+
+            var messages = sqsHelper.GetAllMessages(a.QueueUrls.First()).Result;
+        }
+    }
+}
